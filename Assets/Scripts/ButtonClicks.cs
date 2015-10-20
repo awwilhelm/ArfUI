acf79e9be73d2808bd9356d1e0307d504850a92e
Alex Wilhelm <awwilhelm2014@gmail.com>
2015-10-20 15:54:48 -0500

    Added some functionality to Evidence and Info
    Created basic interface
    Created side view bar for Evidence
    Added dropdown to both Evidence and Info without functionality
    Added a back button

---
+++ Assets/Scripts/ButtonClicks.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonClicks : MonoBehaviour {
+
+    public GameObject panelButton1;
+    public GameObject panelButton2;
+    public GameObject panelButton3;
+    public GameObject panelButton4;
+
+    public GameObject MainPage;
+    public GameObject EvidencePage;
+    public GameObject InfoPage;
+
+    private GameObject currentPage;
+    private GameObject previousPage;
+
+	// Use this for initialization
+	void Start ()
+    {
+        panelButton1.SetActive(true);
+        SetAllPagesToFalse();
+        MainPage.SetActive(true);
+        currentPage = MainPage;
+        previousPage = MainPage;
+    }
+
+    public void SetAllPagesToFalse()
+    {
+        MainPage.SetActive(false);
+        EvidencePage.SetActive(false);
+        InfoPage.SetActive(false);
+    }
+
+    
+
+
+    //Main Menu
+    public void HelpMainMenu()
+    {
+
+    }
+
+    public void EvidenceMainMenu()
+    {
+        MainPage.SetActive(false);
+        EvidencePage.SetActive(true);
+        SetCurrentPage(EvidencePage);
+        SetPreviousPage(MainPage);
+    }
+
+    public void InfoMainMenu()
+    {
+        MainPage.SetActive(false);
+        InfoPage.SetActive(true);
+        SetCurrentPage(EvidencePage);
+        SetPreviousPage(MainPage);
+    }
+
+    public void CallMainMenu()
+    {
+
+    }
+
+    public void ArgumentationMainMenu()
+    {
+
+    }
+
+
+
+    public void GoToPreviousPage()
+    {
+        GameObject temp = GetCurrentPage();
+        SetCurrentPage(GetPreviousPage());
+        SetPreviousPage(temp);
+        SetAllPagesToFalse();
+        GetCurrentPage().SetActive(true);
+    }
+
+    public GameObject GetCurrentPage()
+    {
+        return currentPage;
+    }
+
+    public void SetCurrentPage(GameObject newPage)
+    {
+        currentPage = newPage;
+    }
+
+    public GameObject GetPreviousPage()
+    {
+        return previousPage;
+    }
+
+    public void SetPreviousPage(GameObject newPage)
+    {
+        previousPage = newPage;
+    }
+    
+}
---
+++ Assets/Scripts/EvidencePage.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EvidencePage : MonoBehaviour {
+
+    public GameObject LibrePage;
+    public GameObject PiePage;
+    public GameObject BarPage;
+
+    private ButtonClicks buttonClickScript;
+
+    public enum evidenceOptions
+    {
+        Libre,
+        Pie,
+        Bar,
+        None
+    };
+    private evidenceOptions evidenceActive;
+
+    void Start()
+    {
+        buttonClickScript = GameObject.Find("Canvas").GetComponent<ButtonClicks>();
+
+        evidenceActive = evidenceOptions.None;
+        LibrePage.SetActive(false);
+        PiePage.SetActive(false);
+        BarPage.SetActive(false);
+    }
+
+
+    public void ChangeEvidenceButton(int intButton)
+    {
+        evidenceOptions but = (evidenceOptions)intButton;
+        if(evidenceActive != evidenceOptions.None)
+        {
+            GetGameObjectButton(evidenceActive).SetActive(false);
+        }
+
+        GetGameObjectButton(but).SetActive(true);
+
+        evidenceActive = but;
+    }
+
+    public GameObject GetGameObjectButton(evidenceOptions but)
+    {
+        switch (but)
+        {
+            case evidenceOptions.Libre:
+                return LibrePage;
+            case evidenceOptions.Pie:
+                return PiePage;
+            case evidenceOptions.Bar:
+                return BarPage;
+        }
+        return null;
+    }
+}
